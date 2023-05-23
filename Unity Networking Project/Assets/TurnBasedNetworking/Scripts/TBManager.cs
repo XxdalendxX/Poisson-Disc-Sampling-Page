0e199d82c9951de7af3771ceee9a507f092e0845
XxdalendxX <connordr112@gmail.com>
2023-05-23 15:31:58 +1000

    it's fine... everything is fully under control...

---
+++ Unity Networking Project/Assets/TurnBasedNetworking/Scripts/TBManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+namespace TurnBasedSystem
+{
+    /// <summary>
+    /// This would carry nessesary information for the management of the network system.
+    /// </summary>
+    public class TBManager : NetworkManager
+    {
+        
+    }
+}
---
+++ Unity Networking Project/Assets/TurnBasedNetworking/Scripts/TBPlayer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TurnBasedSystem
+{
+    /// <summary>
+    /// this would carry all the nessesary information for the player to be used as a template for the other player scripts.
+    /// </summary>
+    public class TBPlayer : MonoBehaviour
+    {
+        bool isTurn;
+        
+
+    }
+}
