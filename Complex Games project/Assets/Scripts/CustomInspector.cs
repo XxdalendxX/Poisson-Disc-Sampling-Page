bb096e3978679ae4dd62e220d8939fb762a9d124
XxdalendxX <connordr112@gmail.com>
2023-07-16 15:57:16 +1000

    1) Made it so that you can do a quad space as well as a circle space, 2) allowed the opportunity to have an external piece to run the algorithm in a desired location, 3) made it so you can have core/center anywhere and will change area according to where it is located in world space.

--- Complex Games project/Assets/Scripts/CustomInspector.cs
+++ Complex Games project/Assets/Scripts/CustomInspector.cs
@@ -13,13 +13,37 @@ public class CustomInspector : Editor
         ///Poisson Disc Allignment
         PoissonDiscAllignment pda = (PoissonDiscAllignment)target;
 
+        EditorGUILayout.Space();
+        pda.isCore = GUILayout.Toggle(pda.isCore, "This is the Core Object");
+        if (!pda.isCore)
+        {
+            pda.centerPosition = EditorGUILayout.Vector3Field("Center Position", pda.centerPosition);
+        }
+
+        EditorGUILayout.Space();
+        pda.circleArea = GUILayout.Toggle(pda.circleArea, "Fill a circlular area");
+        if (pda.circleArea)
+        {
+            pda.borderRadius = EditorGUILayout.Slider(pda.borderRadius, 1.0f, 1000.0f);
+        }
+        else
+        {
+            EditorGUILayout.PrefixLabel("X Diameter");
+            pda.XDiameter = EditorGUILayout.FloatField(pda.XDiameter);
+            EditorGUILayout.PrefixLabel("Z Diameter");
+            pda.ZDiameter = EditorGUILayout.FloatField(pda.ZDiameter);
+        }
+
         SphereCollider collider = pda.GetComponent<SphereCollider>();
-        collider.radius = pda.GetObjectRadius();
+        if (collider != null)
+            collider.radius = pda.GetObjectRadius();
 
         if(GUILayout.Button("Perform Poisson Disc Allignment"))
         {
             pda.Execute();
         }
+
+        EditorGUILayout.Space();
 
         if (GUILayout.Button("Clear All Objects"))
         {
--- Complex Games project/Assets/Scripts/PoissonDiscAllignment.cs
+++ Complex Games project/Assets/Scripts/PoissonDiscAllignment.cs
@@ -5,115 +5,114 @@ using UnityEngine;
 public class PoissonDiscAllignment : MonoBehaviour
 {
     [SerializeField] GameObject desiredPrefab;
-    GameObject epicenter;
-
-    [Space]
     [SerializeField] int layerNumber = 0;
-    int layerMask;
 
-    [Header("Radius's")]
-
+    [Header("Object radius")]
     [SerializeField] float objectRadius = 5f;
-    [SerializeField] float borderRadius = 5f;
-
-    [Space]
     [SerializeField, Tooltip("A multiplier put onto the object radius for it's maximum value"), Range(2, 6)] float maxObjectRadiusMultiplier = 5f;
 
     [Space]
     [SerializeField] int attempts = 5;
-    
+
     [Space]
     [SerializeField] List<GameObject> allObjects = new List<GameObject>();
 
+    [Space]
+    ///public variables used in the custom inspector
+    [HideInInspector]public bool isCore = true;
+    [HideInInspector] public Vector3 centerPosition;
+    [HideInInspector] public bool circleArea = true;
+    [HideInInspector] public float borderRadius = 5f;
+    [HideInInspector] public float XDiameter = 0.0f;
+    [HideInInspector] public float ZDiameter = 0.0f;
+
+
+    ///private variables
+    int layerMask;
+    GameObject epicenter;
+
+    /// called from the custom inspector to start the algorithm
+    #region ExecuteFunction
     public void Execute()
     {
-        epicenter = this.gameObject;
         layerMask = LayerMask.GetMask(LayerMask.LayerToName(layerNumber));
-        //StartCoroutine(CoroutinePerform());
-        Perform();
-    }
+        
+        if(isCore)
+        {
+            epicenter = this.gameObject;
 
+            if (circleArea)
+            {
+                PerformCoreCircle();
+            }
+            else
+            {
+                PerformCoreSquare();
+            }
+        }
+        else
+        {
+            if (circleArea)
+            {
+                PerformCorelessCircle();
+            }
+            else
+            {
+                PerformCorelessSquare();
+            }
+        }
+    }
+    #endregion
+
+    /// Clears all objects to save effeciency and trouble
+    #region ClearLists
     public void ClearLists()
     {
-        int obj = allObjects.Count;
-        for (int i = 0; i < obj - 1; i++)
+        if (isCore)
         {
-            DestroyImmediate(allObjects[1]);
-            allObjects.RemoveAt(1);
+            int obj = allObjects.Count;
+            for (int i = 0; i < obj - 1; i++)
+            {
+                DestroyImmediate(allObjects[1]);
+                allObjects.RemoveAt(1);
+            }
+            allObjects.RemoveAt(0);
         }
-        allObjects.RemoveAt(0);
+        else
+        {
+            int obj = allObjects.Count;
+            for (int i = 0; i < obj - 1; i++)
+            {
+                DestroyImmediate(allObjects[1]);
+                allObjects.RemoveAt(1);
+            }
+            GameObject finalObject = allObjects[0];
+            allObjects.RemoveAt(0);
+            GameObject.DestroyImmediate(finalObject);
+        }
     }
+    #endregion
 
-    IEnumerator CoroutinePerform()
+    /// Main Functions that performs the algorithm
+    #region MainFunctions
+    void PerformCoreCircle()
     {
+        //creates core variables
         float y = epicenter.transform.position.y;
         int iterations = 0;
         List<Vector3> openList = new List<Vector3>();
         List<Vector3> closedList = new List<Vector3>();
-
-        openList.Add(epicenter.transform.position);
-        allObjects.Add(epicenter);
-
         bool mustStop = false;
 
-        while(!mustStop)
-        {
-            if (openList.Count < 1)
-            {
-                mustStop = true;
-                break;
-            }
-            Vector3 currentObjectPos = openList[0];
-
-            int fails = 0;
-            while (iterations <= attempts)
-            {
-                
-                Vector3 position = FindLocation(y, currentObjectPos);
-                Collider[] colliders = Physics.OverlapSphere(position, objectRadius, layerMask , QueryTriggerInteraction.Collide);
-                if(colliders.Length == 0 && CheckPosInBorder(position) == true)
-                {
-                    GameObject spawnedObject = SpawnObject(desiredPrefab, position, Quaternion.identity);
-                    SphereCollider sCollider = spawnedObject.GetComponent<SphereCollider>();
-                    sCollider.radius = objectRadius;
-                    openList.Add(spawnedObject.transform.position);
-                    allObjects.Add(spawnedObject);
-                    iterations = 0;
-                }
-                if(colliders.Length > 0)
-                {
-                    fails++;
-                    iterations++;
-                }
-            }
-            Debug.Log("Placement test failed " + fails + " times");
-            iterations = 0;
-            fails = 0;
-            openList.RemoveAt(0);
-            closedList.Add(currentObjectPos);
-            yield return null;
-            //yield return new WaitForSeconds(0.01f);
-        }
-        Debug.Log("PDS complete");
-        yield return null;
-    }
-
-    void Perform()
-    {
-        float y = epicenter.transform.position.y;
-        int iterations = 0;
-        List<Vector3> openList = new List<Vector3>();
-        List<Vector3> closedList = new List<Vector3>();
-
+        //assigns core object in open list and all objects list 
         openList.Add(epicenter.transform.position);
         allObjects.Add(epicenter);
-
-        bool mustStop = false;
 
         while (!mustStop)
         {
             if (openList.Count < 1)
             {
+                //if open list is empty then break the loop
                 mustStop = true;
                 break;
             }
@@ -122,44 +121,307 @@ public class PoissonDiscAllignment : MonoBehaviour
             int fails = 0;
             while (iterations <= attempts)
             {
-
+                //finds a location to perform checks within that meets position critera
                 Vector3 position = FindLocation(y, currentObjectPos);
                 Collider[] colliders = Physics.OverlapSphere(position, objectRadius, layerMask, QueryTriggerInteraction.Collide);
-                if (colliders.Length == 0 && CheckPosInBorder(position) == true)
+                if (colliders.Length == 0 && CheckPosInCircleBorder(position, epicenter.transform.position) == true)
                 {
+                    //places object in world and assigns collider radius
                     GameObject spawnedObject = SpawnObject(desiredPrefab, position, Quaternion.identity);
                     SphereCollider sCollider = spawnedObject.GetComponent<SphereCollider>();
                     sCollider.radius = objectRadius;
+
+                    //adds object transform and gameObject to openList and allObjects respectively and resets the iteration counter  
                     openList.Add(spawnedObject.transform.position);
                     allObjects.Add(spawnedObject);
                     iterations = 0;
                 }
                 if (colliders.Length > 0)
                 {
+                    //position check failed and fail counter and iteration counter goes up
                     fails++;
                     iterations++;
                 }
             }
+            //provides details on open list, closed list and the amount of checks that failed while the object was going through the loop
             Debug.Log("Placement test failed: " + fails + ", || Open List count: " + openList.Count + " || Closed List count: " + closedList.Count );
             iterations = 0;
             fails = 0;
+            //removes transform from open list and transfers it to the closed list
             openList.RemoveAt(0);
             closedList.Add(currentObjectPos);
         }
         Debug.Log("PDS complete");
     }
 
+    void PerformCoreSquare()
+    {
+        //creates core variables
+        float y = epicenter.transform.position.y;
+        int iterations = 0;
+        List<Vector3> openList = new List<Vector3>();
+        List<Vector3> closedList = new List<Vector3>();
+        bool mustStop = false;
+
+        //assigns core object in open list and all objects list 
+        openList.Add(epicenter.transform.position);
+        allObjects.Add(epicenter);
+
+        while (!mustStop)
+        {
+            if (openList.Count < 1)
+            {
+                //if open list is empty then break the loop
+                mustStop = true;
+                break;
+            }
+            Vector3 currentObjectPos = openList[0];
+
+            int fails = 0;
+            while (iterations <= attempts)
+            {
+                //finds a location to perform checks within that meets position critera
+                Vector3 position = FindLocation(y, currentObjectPos);
+                Collider[] colliders = Physics.OverlapSphere(position, objectRadius, layerMask, QueryTriggerInteraction.Collide);
+                if (colliders.Length == 0 && CheckPosInQuadBorder(position, epicenter.transform.position) == true)
+                {
+                    //places object in world and assigns collider radius
+                    GameObject spawnedObject = SpawnObject(desiredPrefab, position, Quaternion.identity);
+                    SphereCollider sCollider = spawnedObject.GetComponent<SphereCollider>();
+                    sCollider.radius = objectRadius;
+
+                    //adds object transform and gameObject to openList and allObjects respectively and resets the iteration counter  
+                    openList.Add(spawnedObject.transform.position);
+                    allObjects.Add(spawnedObject);
+                    iterations = 0;
+                }
+                if (colliders.Length > 0)
+                {
+                    //position check failed and fail counter and iteration counter goes up
+                    fails++;
+                    iterations++;
+                }
+            }
+            //provides details on open list, closed list and the amount of checks that failed while the object was going through the loop
+            Debug.Log("Placement test failed: " + fails + ", || Open List count: " + openList.Count + " || Closed List count: " + closedList.Count);
+            iterations = 0;
+            fails = 0;
+            //removes transform from open list and transfers it to the closed list
+            openList.RemoveAt(0);
+            closedList.Add(currentObjectPos);
+        }
+        Debug.Log("PDS complete");
+    }
+
+    void PerformCorelessCircle()
+    {
+        //creates core variables
+        float y = centerPosition.y;
+        int iterations = 0;
+        List<Vector3> openList = new List<Vector3>();
+        List<Vector3> closedList = new List<Vector3>();
+        bool mustStop = false;
+
+        //Creates and assigns starting object in open list and all objects list if there is an available slot
+        bool gotFirstObject = false;
+        int attempt = 0;
+        while (!gotFirstObject)
+        {
+            Vector3 position;
+            position.x = Random.Range(centerPosition.x - (XDiameter/2) , centerPosition.x + (XDiameter / 2));
+            position.y = y;
+            position.z = Random.Range(centerPosition.z - (ZDiameter / 2), centerPosition.z + (ZDiameter / 2));
+            Collider[] colliders = Physics.OverlapSphere(position, objectRadius, layerMask, QueryTriggerInteraction.Collide);
+            if (CheckPosInCircleBorder(position, centerPosition) == true)
+            {
+                openList.Add(position);
+                GameObject spawnedObject = SpawnObject(desiredPrefab, position, Quaternion.identity);
+                allObjects.Add(spawnedObject);
+                gotFirstObject = true;
+            }
+            else
+            {
+                attempt++;
+            }
+
+            if(attempt > attempts)
+            {
+                gotFirstObject = true;
+                Debug.Log("Failed to find an available location");
+            }
+            
+        }
+
+        while (!mustStop)
+        {
+            if (openList.Count < 1)
+            {
+                //if open list is empty then break the loop
+                mustStop = true;
+                break;
+            }
+            Vector3 currentObjectPos = openList[0];
+
+            int fails = 0;
+            while (iterations <= attempts)
+            {
+                //finds a location to perform checks within that meets position critera
+                Vector3 position = FindLocation(y, currentObjectPos);
+                Collider[] colliders = Physics.OverlapSphere(position, objectRadius, layerMask, QueryTriggerInteraction.Collide);
+                if (colliders.Length == 0 && CheckPosInCircleBorder(position, centerPosition) == true)
+                {
+                    //places object in world and assigns collider radius
+                    GameObject spawnedObject = SpawnObject(desiredPrefab, position, Quaternion.identity);
+                    SphereCollider sCollider = spawnedObject.GetComponent<SphereCollider>();
+                    sCollider.radius = objectRadius;
+
+                    //adds object transform and gameObject to openList and allObjects respectively and resets the iteration counter  
+                    openList.Add(spawnedObject.transform.position);
+                    allObjects.Add(spawnedObject);
+                    iterations = 0;
+                }
+                if (colliders.Length > 0)
+                {
+                    //position check failed and fail counter and iteration counter goes up
+                    fails++;
+                    iterations++;
+                }
+            }
+            //provides details on open list, closed list and the amount of checks that failed while the object was going through the loop
+            Debug.Log("Placement test failed: " + fails + ", || Open List count: " + openList.Count + " || Closed List count: " + closedList.Count);
+            iterations = 0;
+            fails = 0;
+            //removes transform from open list and transfers it to the closed list
+            openList.RemoveAt(0);
+            closedList.Add(currentObjectPos);
+        }
+        Debug.Log("PDS complete");
+    }
+
+    void PerformCorelessSquare()
+    {
+        //creates core variables
+        float y = centerPosition.y;
+        int iterations = 0;
+        List<Vector3> openList = new List<Vector3>();
+        List<Vector3> closedList = new List<Vector3>();
+        bool mustStop = false;
+
+        //assigns core object in open list and all objects list 
+        bool gotFirstObject = false;
+        int attempt = 0;
+        while (!gotFirstObject)
+        {
+            Vector3 position;
+            position.x = Random.Range(centerPosition.x - (XDiameter / 2), centerPosition.x + (XDiameter / 2));
+            position.y = y;
+            position.z = Random.Range(centerPosition.z - (ZDiameter / 2), centerPosition.z + (ZDiameter / 2));
+            Collider[] colliders = Physics.OverlapSphere(position, objectRadius, layerMask, QueryTriggerInteraction.Collide);
+            if (CheckPosInCircleBorder(position, centerPosition) == true)
+            {
+                openList.Add(position);
+                GameObject spawnedObject = SpawnObject(desiredPrefab, position, Quaternion.identity);
+                allObjects.Add(spawnedObject);
+                gotFirstObject = true;
+            }
+            else
+            {
+                attempt++;
+            }
+
+            if (attempt > attempts)
+            {
+                gotFirstObject = true;
+                Debug.Log("Failed to find an available location");
+            }
+
+        }
+
+        while (!mustStop)
+        {
+            if (openList.Count < 1)
+            {
+                //if open list is empty then break the loop
+                mustStop = true;
+                break;
+            }
+            Vector3 currentObjectPos = openList[0];
+
+            int fails = 0;
+            while (iterations <= attempts)
+            {
+                //finds a location to perform checks within that meets position critera
+                Vector3 position = FindLocation(y, currentObjectPos);
+                Collider[] colliders = Physics.OverlapSphere(position, objectRadius, layerMask, QueryTriggerInteraction.Collide);
+                if (colliders.Length == 0 && CheckPosInQuadBorder(position, centerPosition) == true)
+                {
+                    //places object in world and assigns collider radius
+                    GameObject spawnedObject = SpawnObject(desiredPrefab, position, Quaternion.identity);
+                    SphereCollider sCollider = spawnedObject.GetComponent<SphereCollider>();
+                    sCollider.radius = objectRadius;
+
+                    //adds object transform and gameObject to openList and allObjects respectively and resets the iteration counter  
+                    openList.Add(spawnedObject.transform.position);
+                    allObjects.Add(spawnedObject);
+                    iterations = 0;
+                }
+                if (colliders.Length > 0)
+                {
+                    //position check failed and fail counter and iteration counter goes up
+                    fails++;
+                    iterations++;
+                }
+            }
+            //provides details on open list, closed list and the amount of checks that failed while the object was going through the loop
+            Debug.Log("Placement test failed: " + fails + ", || Open List count: " + openList.Count + " || Closed List count: " + closedList.Count);
+            iterations = 0;
+            fails = 0;
+            //removes transform from open list and transfers it to the closed list
+            openList.RemoveAt(0);
+            closedList.Add(currentObjectPos);
+        }
+        Debug.Log("PDS complete");
+    }
+    #endregion
+
+    /// Miscilaneous Functions to help perform
+    #region MiscelaneousFunctions
     public float GetObjectRadius()
     {
         return objectRadius;
     }
 
-    bool CheckPosInBorder(Vector3 position)
+    bool CheckPosInCircleBorder(Vector3 position, Vector3 corePosition)
     {
+        float distance = Mathf.Sqrt(Mathf.Pow(position.x - corePosition.x,2) + Mathf.Pow(position.z - corePosition.z,2));
         float radius = Mathf.Sqrt(Mathf.Pow(position.x, 2) + Mathf.Pow(position.z, 2));
-        if(radius <= borderRadius)
+        if(distance <= borderRadius)
         {
             return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool CheckPosInQuadBorder(Vector3 position, Vector3 corePos)
+    {
+        float XMax = corePos.x + (XDiameter / 2);
+        float XMin = corePos.x - (XDiameter / 2);
+        float ZMax = corePos.z + (ZDiameter / 2);
+        float ZMin = corePos.z - (ZDiameter / 2);
+
+        if (position.x <= XMax && position.x >= XMin)
+        {
+            if (position.z <= ZMax && position.z >= ZMin)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         else
         {
@@ -182,4 +444,5 @@ public class PoissonDiscAllignment : MonoBehaviour
     {
         return Instantiate(gameObject, position, rotation);
     }
+    #endregion
 }
